4ea9470d784c60200cbc16c52d04afcbfd041d75
kingstar <admin@baks.dev>
2024-09-16 01:39:15 +0300

    Baks Development

---
+++ Resources/config/doctrine.php
@@ -0,0 +1,47 @@
+<?php
+/*
+ *  Copyright 2023.  Baks.dev <admin@baks.dev>
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is furnished
+ *  to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ */
+
+namespace Symfony\Component\DependencyInjection\Loader\Configurator;
+
+use BaksDev\Yandex\Market\BaksDevYandexMarketBundle;
+use BaksDev\Yandex\Market\Type\Company\YaMarketTokenCompanyType;
+use BaksDev\Yandex\Market\Type\Company\YaMarketTokenCompanyUid;
+use BaksDev\Yandex\Market\Type\Event\YaMarketTokenEventType;
+use BaksDev\Yandex\Market\Type\Event\YaMarketTokenEventUid;
+use Symfony\Config\DoctrineConfig;
+
+return static function (DoctrineConfig $doctrine, ContainerConfigurator $configurator): void {
+
+    $doctrine->dbal()->type(YaMarketTokenEventUid::TYPE)->class(YaMarketTokenEventType::class);
+    $doctrine->dbal()->type(YaMarketTokenCompanyUid::TYPE)->class(YaMarketTokenCompanyType::class);
+
+    $emDefault = $doctrine->orm()->entityManager('default')->autoMapping(true);
+
+    $emDefault
+        ->mapping('yandex-market')
+        ->type('attribute')
+        ->dir(BaksDevYandexMarketBundle::PATH.'Entity')
+        ->isBundle(false)
+        ->prefix(BaksDevYandexMarketBundle::NAMESPACE.'\\Entity')
+        ->alias('yandex-market');
+};
