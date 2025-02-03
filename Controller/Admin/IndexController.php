18352c55908833752e457a9e27cfdfcb1ad3e228
kingstar <admin@baks.dev>
2025-02-03 15:27:17 +0300

    Baks Development

--- Controller/Admin/IndexController.php
+++ Controller/Admin/IndexController.php
@@ -49,11 +49,12 @@ final class IndexController extends AbstractController
 
         // Поиск
         $search = new SearchDTO();
+
         $searchForm = $this
             ->createForm(
-                SearchForm::class,
-                $search,
-                ['action' => $this->generateUrl('yandex-market:admin.index')]
+                type: SearchForm::class,
+                data: $search,
+                options: ['action' => $this->generateUrl('yandex-market:admin.index')]
             )
             ->handleRequest($request);
 
--- Controller/Admin/Tests/EditControllerTest.php
+++ Controller/Admin/Tests/EditControllerTest.php
@@ -1,19 +1,24 @@
 <?php
 /*
- *  Copyright 2022.  Baks.dev <admin@baks.dev>
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *  http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *   limitations under the License.
- *
+ *  Copyright 2025.  Baks.dev <admin@baks.dev>
+ *  
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is furnished
+ *  to do so, subject to the following conditions:
+ *  
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *  
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
  */
 
 namespace BaksDev\Yandex\Market\Controller\Admin\Tests;
@@ -29,9 +34,9 @@ use Symfony\Component\DependencyInjection\Attribute\When;
 #[When(env: 'test')]
 final class EditControllerTest extends WebTestCase
 {
-    private const URL = '/admin/ya/market/token/edit/%s';
+    private const string URL = '/admin/ya/market/token/edit/%s';
 
-    private const ROLE = 'ROLE_YA_MARKET_TOKEN_EDIT';
+    private const string ROLE = 'ROLE_YA_MARKET_TOKEN_EDIT';
 
 
     private static ?YaMarketTokenEventUid $identifier = null;
