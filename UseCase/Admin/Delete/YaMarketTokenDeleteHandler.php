5534a7091336454e2879118b6f0a1764107f2421
MichelAngelo <baksdevelopmen@gmail.com>
2025-12-09 01:38:15 +0300

    BaksDevelopment

--- UseCase/Admin/Delete/YaMarketTokenDeleteHandler.php
+++ UseCase/Admin/Delete/YaMarketTokenDeleteHandler.php
@@ -1,17 +1,17 @@
 <?php
 /*
- *  Copyright 2023.  Baks.dev <admin@baks.dev>
- *
+ *  Copyright 2025.  Baks.dev <admin@baks.dev>
+ *  
  *  Permission is hereby granted, free of charge, to any person obtaining a copy
  *  of this software and associated documentation files (the "Software"), to deal
  *  in the Software without restriction, including without limitation the rights
  *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  *  copies of the Software, and to permit persons to whom the Software is furnished
  *  to do so, subject to the following conditions:
- *
+ *  
  *  The above copyright notice and this permission notice shall be included in all
  *  copies or substantial portions of the Software.
- *
+ *  
  *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  *  FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
@@ -29,26 +29,13 @@ use BaksDev\Core\Entity\AbstractHandler;
 use BaksDev\Yandex\Market\Entity\Event\YaMarketTokenEvent;
 use BaksDev\Yandex\Market\Entity\YaMarketToken;
 use BaksDev\Yandex\Market\Messenger\YaMarketTokenMessage;
-use DomainException;
 
 final class YaMarketTokenDeleteHandler extends AbstractHandler
 {
     public function handle(YaMarketTokenDeleteDTO $command): string|YaMarketToken
     {
-        /* Валидация DTO  */
-        $this->validatorCollection->add($command);
-
-        $this->main = new YaMarketToken($command->getProfile());
-        $this->event = new YaMarketTokenEvent();
-
-        try
-        {
-            $this->preRemove($command);
-        }
-        catch(DomainException $errorUniqid)
-        {
-            return $errorUniqid->getMessage();
-        }
+        $this->setCommand($command)
+            ->preEventRemove(YaMarketToken::class, YaMarketTokenEvent::class);
 
         /* Валидация всех объектов */
         if($this->validatorCollection->isInvalid())
@@ -56,7 +43,7 @@ final class YaMarketTokenDeleteHandler extends AbstractHandler
             return $this->validatorCollection->getErrorUniqid();
         }
 
-        $this->entityManager->flush();
+        $this->flush();
 
         /* Отправляем сообщение в шину */
         $this->messageDispatch->dispatch(
